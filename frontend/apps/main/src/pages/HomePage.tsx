292ac041eae82cf29c7772b1f9a6151953356eb3
Fedosov Alex <alexfedosov1985@gmail.com>
2026-02-04 06:57:02 +0300

    1.6.6.5 Small ui fix

--- frontend/apps/main/src/pages/HomePage.tsx
+++ frontend/apps/main/src/pages/HomePage.tsx
@@ -1045,8 +1045,8 @@ export function HomePage() {
               </h1>
             </div>
             <div className="home-hero-panel">
-              <div className="home-hero-panel-title">Математика 3 класс <br /> через<br /></div>
-              {<Countdown targetIso={TARGET_DATE} /> }
+              <div className="home-hero-panel-title">Математика 3 класс <br /> доступна для прохождения<br /></div>
+              {/*<Countdown targetIso={TARGET_DATE} /> */}
               { <Button onClick={() => handleStartSubject("math")}>Принять участие</Button> }
             </div>
           </div>
